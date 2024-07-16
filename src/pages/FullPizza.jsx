47f719ba17dd1a1a927f6d13c6cbd188bfb6c412
Ruzimurod11 <donievruzimurod@gmail.com>
2024-07-16 10:15:46 +0500

    My ts project

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const FullPizza = () => {
+	const [pizza, setPizza] = useState();
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		async function fetchPizza() {
+			try {
+				const { data } = await axios.get(
+					"https://6688719f0ea28ca88b85405a.mockapi.io/items/" + id,
+				);
+				setPizza(data);
+			} catch (error) {
+				alert("Ошибка при получении пиццы!");
+				navigate("/");
+			}
+		}
+
+		fetchPizza();
+	}, []);
+
+	if (!pizza) {
+		return "Загрузка...";
+	}
+	return (
+		<div className="container cart__info--container">
+			<img
+				src={pizza.imageUrl}
+				alt="pizza"
+			/>
+			<div className="cart__info--card">
+				<h2 className="cart__info--title">{pizza.title}</h2>
+				<h4 className="cart__info--price">{pizza.price} ₽</h4>
+			</div>
+		</div>
+	);
+};
+
+export default FullPizza;
